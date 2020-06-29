e79dcf4159cf57d9e66d4c584de7f73a6aadf874
Gethyn Jones <gethyn1@gmail.com>
2020-06-29 11:26:17 +0100

    Add theme

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Container, Heading, Button, Flex } from 'theme-ui'
 
 export default () => {
   return (
-    <h1>The index</h1>
+    <Container>
+      <Flex sx={{ flexDirection: "column", padding: 3 }}>
+        <Heading as="h1">Save some evidence</Heading>
+        <Button
+          sx={{ marginTop: 2 }}
+          onClick={() => {
+            alert("clicked")
+          }}
+        >Log In</Button>
+      </Flex>
+    </Container>
   )
 }
